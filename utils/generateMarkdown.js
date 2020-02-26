a5808c60dfeb6ab3db2d52e3739895c15216b7f8
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-02-25 19:13:19 -0700

    Add table of contents to generated readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 module.exports = {
     generateMarkdown: function(data) {
-        return ` # ${data.projectName} \n ## Description \n ${data.description} \n ## Installation \n ${data.install} \n ## Usage \n ${data.usage} \n ## License \n ${data.license} \n ## Contributing \n ${data.contrib} \n ## Tests \n ${data.tests} \n ## Questions \n github info will go here`;
+        return ` # ${data.projectName} \n ## Description \n ${data.description} \n ## Table of Contents \n * [Installation](#installation) \n * [Usage](#usage) \n * [License](#license) \n * [Contributing](#contributing) \n * [Tests](#tests) \n * [Questions](#questions) \n ## Installation \n ${data.install} \n ## Usage \n ${data.usage} \n ## License \n ${data.license} \n ## Contributing \n ${data.contrib} \n ## Tests \n ${data.tests} \n ## Questions \n github info will go here`;
     }
 };
