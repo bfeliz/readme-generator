9de5d5b2daf3bfae8f54389f1f6ee7dc4623f697
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-02-25 17:29:08 -0700

    Begin inquirer prompts, launch generateMarkdown function

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const questions = [
+const inquirer = require("inquirer");
+let mark = require("./utils/generateMarkdown.js");
+const fs = require("fs");
 
-];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "githubName",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "projectName",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Type a description of your project:"
+        }
+    ])
+    .then(function(data) {
+        mark.generateMarkdown(data);
+        // let returnedData = JSON.stringify(data, null, "\t");
+        // console.log(returnedData);
+        // module.exports = returnedData;
+    });
 
-function writeToFile(fileName, data) {
-}
+const questions = [];
 
-function init() {
+function writeToFile(fileName, data) {}
 
-}
+function init() {}
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,15 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
+module.exports = {
+    generateMarkdown: function(data) {
+        let stringData = JSON.stringify(data, null, 2);
+        console.log(stringData);
+    }
+};
 
-`;
-}
+// function generateMarkdown(data) {
+//     //     return `
+//     // # ${data.title}
 
-module.exports = generateMarkdown;
+//     // `;
+// }
+
+// module.exports = generateMarkdown;
